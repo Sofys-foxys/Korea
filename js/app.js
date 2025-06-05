4581498a086e6b5e7a88ecc13f22348ee5fbac30
Sofys-foxys <sofia.castroe@gmail.com>
2025-06-05 14:20:03 +0200

    Added extra elements

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('App loaded');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('App loaded');
+  
+  const toggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('.navbar');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
